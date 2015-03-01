543b2d156f922878cc4cb7c54437f48a6e98d64b
luismoramedina <luismoramedina@gmail.com>
2015-03-01 22:25:02 +0100

    a spring boot app

---
+++ src/main/java/demo/HelloController.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+}
