6168320fe81051815d29dee1da2f1f104c8b52fc
luismoramedina <luismoramedina@gmail.com>
2015-03-28 20:49:26 +0100

    NEW add context file to springboot app

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -2,11 +2,25 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@ImportResource("classpath*:*-context*.xml")
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            if (beanName.startsWith("test")) {
+               System.out.println("#####################" + beanName);
+            }
+        }
+
     }
 }
